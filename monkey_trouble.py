bdb412db671968605b7628945bd677b11185fabd
Mohit Mehta <mohitbmehta07@gmail.com>
2018-03-23 15:51:35 -0700

    Create monkey_trouble.py

---
+++ monkey_trouble.py
@@ -0,0 +1,9 @@
+def monkey_trouble(a_smile, b_smile):
+    if a_smile == True and b_smile == True:
+      return True
+    elif a_smile == False and b_smile == False:
+      return True
+    elif a_smile == True and b_smile == False:
+      return False
+    else:
+      return False
